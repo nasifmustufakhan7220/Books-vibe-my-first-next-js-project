7fbac5f48c2df209d873d22857b71ae38b7d9c72
Nasif Khan <nasifmustufakhan7220@gmail.com>
2026-09-20 16:12:27 +0600

    navbar and banner design completed with responsive and then take books data from our local through fetch and then make a Book card component  to show each card

--- src/components/Shared/Navbar.tsx
+++ src/components/Shared/Navbar.tsx
@@ -46,7 +46,7 @@ const Navbar = () => {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-1 shadow"
             >{links}</ul>
           </div>
-          <div className="btn btn-ghost text-xl">
+          <div className="text-xl">
             <Image src={logo} alt="book logo"></Image>
           </div>
         </div>
